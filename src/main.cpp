404aff6da6b902b0379819ea7479c3c5fc6a8a80
3dgami <>
2023-11-04 18:20:16 -0700

    inverted launch motors to turn correct direction
    corrected arcade control
    
    now added working pnuematic controls

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,9 @@
 #include "main.h"
 
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {}
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-
 pros::Controller master{CONTROLLER_MASTER};	
 pros::Motor intake(14);
-pros::Motor launchN(12);	//update all motor ports
+pros::Motor launchN(12, true);	//update all motor ports
 pros::Motor launchP(13);
 pros::Motor right_front(11);
 pros::Motor left_front(1);
@@ -210,45 +149,114 @@ void turn(int angle)
 	return;
 }
 
+/**
+ * A callback function for LLEMU's center button.
+ *
+ * When this callback is fired, it will toggle line 2 of the LCD text between
+ * "I was pressed!" and nothing.
+ */
+void on_center_button() {}
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
 
 void opcontrol()
 {
-
-	pros::ADIAnalogOut piston (1);//get port
+	int turned = 0;
+	double multiply = 0.007 * turned;
+	auto ExpansionPort = 'A';
+	auto ExpansionIntakePort = 'B';
+	pros::c::adi_pin_mode(ExpansionPort, OUTPUT);
+	pros::c::adi_digital_write(ExpansionPort, LOW);
+	pros::c::adi_pin_mode(ExpansionIntakePort, OUTPUT);
+	pros::c::adi_digital_write(ExpansionIntakePort, LOW);
+	//pros::ADIAnalogOut piston (1);//get port
 	pros::Controller master(CONTROLLER_MASTER);
-	launchN.set_reversed(true);
+	//launchN.set_reversed(true);
+
 
 	bool intakeState = false;
 	bool extend = false;
+	bool extendIntake = false;
 	int dead_Zone = 10;
 
 	while(true)
 	{
+		//driveR_train.set_reversed(true);
+		//driveL_train.move(master.get_analog(ANALOG_LEFT_Y));
+		//driveR_train.move(master.get_analog(ANALOG_RIGHT_Y));
 		
-		driveL_train.move(master.get_analog(ANALOG_LEFT_Y));
-		driveR_train.move(master.get_analog(ANALOG_RIGHT_Y));
-		
-		/*
-		ARCADE CONTROLL
+		/*ARCADE CONTROLL*/
 
-		int power = master.get_analog(ANALOG_LEFT_Y);
-		int turn = master.get_analog(ANALOG_RIGHT_X);
-		int left = power + turn;
-		int right = power - turn;
+		int power = master.get_analog(ANALOG_RIGHT_X);
+		int turn = master.get_analog(ANALOG_LEFT_Y);
+		int left = power - turn;
+		int right = power + turn;
 		driveL_train.move(left);
 		driveR_train.move(right);
-		*/
+		
+		
 	
 		if (master.get_digital_new_press(DIGITAL_L1))
 		{
 			if (extend == true)
 			{
-				piston.set_value(false);
+				pros::c::adi_digital_write(ExpansionPort, LOW);
 				extend = false;
 			}
 			else
 			{
-				piston.set_value(true);
+				pros::c::adi_digital_write(ExpansionPort, HIGH);
 				extend = true;
 			}
 		
@@ -257,9 +265,13 @@ void opcontrol()
 
 		if (master.get_digital_new_press(DIGITAL_R1))
 		{
-			launchN.move_relative(900 * 3, 100);
-			launchP.move_relative(900 * 3, 100);
-			printf("Digital_R1 launch \n");
+
+			launchN.move_relative(1800 * (1 + multiply), 150);// had times 3 // 100
+			launchP.move_relative(1800 * (1 + multiply), 150);
+			turned += 1;
+			double multiply = 0.006 * turned;
+			printf("Digital_R1 launch, turned=%d, multiply=%f\n", turned, multiply);
+
 		}
 
 		if (master.get_digital_new_press(DIGITAL_Y))
@@ -281,7 +293,21 @@ void opcontrol()
 
 		if (master.get_digital_new_press(DIGITAL_X))
 		{
+
+			if (extendIntake == true)
+			{
+				pros::c::adi_digital_write(ExpansionIntakePort, LOW);
+				extendIntake = false;
+			}
+			else
+			{
+				pros::c::adi_digital_write(ExpansionIntakePort, HIGH);
+				extendIntake = true;
+			}
+		
+			printf("Digital_X Pnuematic Intake, Extend=%d \n", extend);
 			printf("Digital_X \n");
+
 		}
 
 		pros::delay(10);
