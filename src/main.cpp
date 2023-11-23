5094c130240a8e141c79cd84cf333aaf243af2e4
3dgami <69698462+3dgami@users.noreply.github.com>
2023-11-23 11:09:13 -0800

    Update main.cpp
    fixed negative input problems for drivetrain and turn functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
 #include "main.h"
 
+
 pros::Controller master{CONTROLLER_MASTER};	
-pros::Motor intake1(1);
-pros::Motor intake2(10, true);
+pros::Motor intake1(19);
+//pros::Motor intake2(10, true);
 pros::Motor launchN(14, true);	//update all motor ports
 pros::Motor launchP(15);
-pros::Motor right_front(11);
-pros::Motor left_front(20);
-pros::Motor left_back(19);//
-pros::Motor right_back(12);
+pros::Motor right_front(20);
+pros::Motor left_front(10);
+pros::Motor left_back(1);//
+pros::Motor right_back(11);
 pros::Motor_Group driveL_train({left_front, left_back});
 pros::Motor_Group driveR_train({right_front, right_back});
-int rotationPort = 16;
+int rotationPort = 11;
 int maxAngle = -10;
 int minAngle = 1000000000;
-int ShootPos = 12400;
+int ShootPos = 11800;
 int UpPos = 4702;
+
 
 void SetDriveRelative(int ticks, int Lspeed, int Rspeed)
 	{
@@ -28,23 +30,6 @@ void SetDriveRelative(int ticks, int Lspeed, int Rspeed)
 		//right_middle.move_relative(ticks, Rspeed);
 		right_back.move_relative(ticks, Rspeed);
 	}
-
-pros::Controller master{CONTROLLER_MASTER};
-pros::Motor intake(14);
-pros::Motor launchN(12); // update all motor ports
-pros::Motor launchP(13);
-pros::Motor right_front(11);
-pros::Motor left_front(1);
-pros::Motor left_back(10);
-pros::Motor right_back(20);
-pros::Motor_Group driveL_train({left_front, left_back});
-pros::Motor_Group driveR_train({right_front, right_back});
-
-void driveU_train(int Rspeed, int Lspeed)
-{
-	driveL_train.move(Lspeed);
-	driveR_train.move(Rspeed);
-}
 
 void SetDrive(int Lspeed, int Rspeed)
 	{
@@ -84,11 +69,11 @@ void driveTrain(int distance)
 
 	driveL_train.set_reversed(true);
 	int startPos = getPos();
-	double kp = 15.0;
+	double kp = 12.0;
 	double ki = 0.2;
-	double kd = -0.15;
-	/* derivitive should control and stop overshooting this can be done
-	   by having kd be negative or having a (P + I - D) for the output PS */
+	double kd = -0.15;   /*derivitive should control and stop overshooting this can be done
+						  by having kd be negative or having a (P + I - D) for the output PS 
+						*/
 	double P;
 	double I;
 	double D;
@@ -97,12 +82,13 @@ void driveTrain(int distance)
 	int errorTotal = 0;
 	int sign;
 
-	if (distance < 0) {
+	if (distance < 0){
 		sign = -1;
-	} else {
-		sign = 1;
-	}
-
+		}
+	else{
+			sign = 1;
+		}
+	
 	errorTerm = distance + startPos - getPos();
 
 	while (errorTerm > 1 or errorTerm < -1)
@@ -118,10 +104,10 @@ void driveTrain(int distance)
 			errorTotal = 50 / ki;
 		}
 
+
 		P = errorTerm * kp;
 		//I = errorTotal * ki;
 		D = (lastError - errorTerm) * kd;
-    
 		int output = (((P + D) + (600*sign)));
 
 		printf("O=%D, P=%0.2f, D=%0.2f, Position=%d, startPos=%d Err=%d\n",output, P, D, Pos, startPos, errorTerm);
@@ -145,24 +131,37 @@ void turn(int angle)
 	double CircleTicks = 2450;
 	int turnTicks = (CircleTicks/360) * angle;
 
+
+
 	int startPos = getPos();
 	double kp = 9.0;
-	// double ki = 0.2;
+	//double ki = 0.2;
 	double kd = -0.05; /*derivitive should control and stop overshooting this can be done
 						  by having kd be negative or having a (P + I - D) for the output
 						*/
 	double P;
-	// double I;
+	//double I;
 	double D;
 	int lastError = 0;
 	int errorTerm;
 	int errorTotal = 0;
 	int sign = 1;
 
+	if (angle < 0)
+	{
+		sign = -1;
+	}
+	else
+	{
+		sign = 1;
+	}
+
 	printf("start\n");
-	while (errorTerm > 0)
+	while (errorTerm > 1 or errorTerm < -1)
 	{
 		errorTerm = (turnTicks + startPos) - floor(getPos());
+
+		
 
 
 		int pos = getPos();
@@ -173,12 +172,12 @@ void turn(int angle)
 		{
 			errorTotal = 50 / ki;
 		}
-		*/
+		*/	
 
 		P = errorTerm * kp;
-		// I = errorTotal * ki;
+		//I = errorTotal * ki;
 		D = (lastError - errorTerm) * kd;
-		int output = (P + D) + 1100;
+		int output = ((P + D) + (1100*sign));
 
 		printf("step err=%d, P=%.02f, D=%.02f, StartPos=%d, Pos=%d, O=%d\n, turn=%d", errorTerm, P, D, startPos, pos, output, turnTicks);
 
@@ -273,82 +272,34 @@ void autonomous()
 	pros::c::adi_pin_mode(ExpansionIntakePort, OUTPUT);
 	pros::c::adi_digital_write(ExpansionIntakePort, LOW);
 	
-	//Move(-1000, 100, 100, 10000);
-	driveTrain(-500);
-	pros::delay(1000);
-	turn(45);
-	pros::delay(1000);
-	//Move(-250, 100, 100, 10000);
-	//driveTrain(-500);
-	pros::delay(1000);
+	//driveTrain(-1250);//one block
+	driveTrain(1000);
+	pros::delay(500);
+	turn(-45);
+	pros::delay(500);
+	driveTrain(1000);
+	pros::delay(500);
+	//driveTrain(-200);
+	//pros::delay(500);
+	//driveTrain(250);
+	//pros::delay(500);
+	driveTrain(-400);
 
-	//get alliance triball in goal
-
-	/*driveTrain(100);
-	pros::delay(100);
-	turn(90);
-	pros::delay(100);
-	driveTrain(-(1450*1.5));
-	pros::delay(100);
+	pros::delay(500);
 	turn(-90);
-	pros::delay(100);
-	//move to center for first field triball
-
-	intake1.move_velocity(-200);
-	intake2.move_velocity(-200);
-	pros::delay(100);
-	driveTrain(-(1450));
-	//pros::delay(50);
-	intake1.move_velocity(0);
-	intake2.move_velocity(0);
-	pros::delay(250);
-	//collect first field triball into bot
-
+	pros::delay(500);
+	driveTrain(1250);
+	pros::delay(500);
 	turn(90);
-	pros::delay(100);
-	driveTrain(-(1450));
-	intake1.move_velocity(200);
-	intake2.move_velocity(200);
-	pros::delay(250);
-	intake1.move_velocity(0);
-	intake2.move_velocity(0);
-	//get first field triball into goal
+	pros::delay(500);
+	driveTrain(1250);
 
-	turn(180);
-	pros::delay(100);
-	intake1.move_velocity(-200);
-	intake2.move_velocity(-200);
-	driveTrain(-(1450*1.75));
-	//pros::delay(50);
-	intake1.move_velocity(0);
-	intake2.move_velocity(0);
-	//turn around to and move to get second field triball into bot
 
-	pros::delay(100);
-	turn(180);
-	pros::delay(100);
-	driveTrain(-(1450*1.75));
-	intake1.move_velocity(200);
-	intake2.move_velocity(200);
-	pros::delay(100);
-	//move second field triball into goal
-
-	pros::c::adi_digital_write(ExpansionPort, HIGH);
-	driveTrain(250);
-	turn(-20);
-	driveTrain(-(250));
-	driveTrain(250);
-	turn(40);
-	driveTrain(-(350));
-	driveTrain(400);
-	//extend wing and bump into goal two times at different angles to ensure triballs are in goal
-
-	*/
 	//shut down all motors
 	//intake1.move_velocity(0);
 	//intake2.move_velocity(0);
-	driveR_train.move_voltage(0);
-	driveL_train.move_voltage(0);
+	//driveR_train.move_voltage(0);
+	//driveL_train.move_voltage(0);
 
 	//pros::c::adi_digital_write(ExpansionPort, LOW);
 
@@ -382,30 +333,25 @@ void opcontrol()
 
 	pros::Rotation rotation_sensor(rotationPort);
 
-	pros::ADIAnalogOut piston(1); // get port
 	pros::Controller master(CONTROLLER_MASTER);
 
 	//rotation_sensor.set_position(0);
 
 	bool intakeState = false;
-	bool extended = false;
+	bool extend = true;//change to false
+	bool extendIntake = false;
 
 	int dead_Zone = 10;
 	int count = 0;
 
-	pros::delay(500);
+	//pros::delay(500);
 	//driveTrain(1500);
-	turn(90);
-	pros::delay(500);
+	//turn(90);
+	//pros::delay(500);
 
-	while (true)
+	while(true)
 	{
-		driveL_train.move(master.get_analog(ANALOG_LEFT_Y));
-		driveR_train.move(master.get_analog(ANALOG_RIGHT_Y));
 
-		/*
-		ARCADE CONTROLL
-=======
 		rotation_sensor.get_angle();
 		int angle = rotation_sensor.get_angle();
 		if(angle > maxAngle)
@@ -437,64 +383,46 @@ void opcontrol()
 
 		driveL_train.move(left);
 		driveR_train.move(right);
-    
-		*/
-
-		if (master.get_digital_new_press(DIGITAL_L1)) {
-			/*
-			turn pneumatic on / off & keep track of state as PROS 
-			doesn't do this for us already.
-			
-			NOTE: the way that Vova wrote the code last year used the higher-level 
-			pros::c::adi_digital_write(port_name, port_state) function, which bypasses 
-			the object-oriented pros::ADIAnalogOut protocols. This same code would look like:
-			
-			```cpp
-			if (extended) {
-				pros::c::adi_digital_write(1, false);
-			    extended = false;
-			} else {
-			    pros::c::adi_digital_write(1, true);
-			   extended = true;
+		
+		
+	
+		if (master.get_digital_new_press(DIGITAL_L1))
+		{
+			if (extend == true)
+			{
+				pros::c::adi_digital_write(ExpansionPort, LOW);
+				extend = false;
 			}
-			```
-			
-			not sure why the function protocol worked for us last year but try it out. 
-			(obviously the port number should be renamed as a variable or macro but that's 
-			for readability.) 
-			*/
-
-			if (extended) {
-				piston.set_value(false);
-				extended = false;
-			} else {
-				piston.set_value(true);
-				extended = true;
+			else
+			{
+				pros::c::adi_digital_write(ExpansionPort, HIGH);
+				extend = true;
 			}
-
-			printf("Digital_L1 Pnuematic, Extend=%d \n", extended);
+		
+			printf("Digital_L1 Pnuematic, Extend=%d \n", extend);
 		}
 
 		if (master.get_digital_new_press(DIGITAL_R1) && extend == true)
 		{	
 
-			launchN.move_relative(200, 100);
-			launchP.move_relative(200, 100);
+			launchN.move_relative(150, 100);
+			launchP.move_relative(150, 100);
 			//angle = rotation_sensor.get_angle();
-			pros::delay(500);// Ill try to lower this delay but the get_angle sometime doesnt get the end angle if no delay, but ill have to test
+			pros::delay(250);// Ill try to lower this delay but the get_angle sometime doesnt get the end angle if no delay, but ill have to test
 			angle = rotation_sensor.get_angle();
 
 			while(angle < (ShootPos))
-			{
-				launchN.move_velocity(100);
-				launchP.move_velocity(100);
-				angle = rotation_sensor.get_angle();
-				printf("angle=%d \n", angle);
+			{	
 				if(angle >= (ShootPos))
 				{
 					break;
 				}
-				pros::delay(10);
+				angle = rotation_sensor.get_angle();
+				launchN.move_velocity(150);
+				launchP.move_velocity(150);
+				
+				printf("angle=%d \n", angle);
+				pros::delay(5);
 
 			}
 			launchN.move_velocity(0);
@@ -509,7 +437,7 @@ void opcontrol()
 			if (intakeState == false)
 			{
 				intake1.move_velocity(-200);
-				intake2.move_velocity(-200);
+				//intake2.move_velocity(-200);
 
 				intakeState = true;
 
@@ -518,7 +446,7 @@ void opcontrol()
 			else
 			{
 				intake1.move_velocity(0);
-				intake2.move_velocity(0);
+				//intake2.move_velocity(0);
 
 				intakeState = false;
 
@@ -526,26 +454,27 @@ void opcontrol()
 			}
 		}
 
-		if (master.get_digital_new_press(DIGITAL_Y)) {
+		if (master.get_digital_new_press(DIGITAL_Y))
+		{	
 			if (intakeState == false)
 			{
 				intake1.move_velocity(200);
-				intake2.move_velocity(200);
+				//intake2.move_velocity(200);
 
 				intakeState = true;
 
 				printf("intakeState = true");
 			}
 			else
-			\
+			{
 				intake1.move_velocity(0);
-				intake2.move_velocity(0);
+				//intake2.move_velocity(0);
 
 				intakeState = false;
 
 				printf("intakeState = false");
 			}
-
+		
 			printf("Digital_Y intake intakeState=%d \n", intakeState);
 		}
 
@@ -559,7 +488,7 @@ void opcontrol()
 				pros::c::adi_digital_write(ExpansionIntakePort, LOW);
 
 				intake1.move_velocity(0);
-				intake2.move_velocity(0);
+				//intake2.move_velocity(0);
 
 				extendIntake = false;
 			}
@@ -572,12 +501,12 @@ void opcontrol()
 			printf("Digital_X Pnuematic Intake, Extend=%d \n", extend);
 
 		}
-		if (master.get_digital_new_press(DIGITAL_RIGHT) && extend == true)
+		if (master.get_digital_new_press(DIGITAL_RIGHT) && extend)
 		{
 			
 		}
 
-		if (master.get_digital_new_press(DIGITAL_LEFT) && extend == true)
+		if (master.get_digital_new_press(DIGITAL_LEFT) && extend)
 		{
 			launchN.move_relative(100, 50);
 			launchP.move_relative(100, 50);
@@ -585,7 +514,7 @@ void opcontrol()
 
 		while (master.get_digital(DIGITAL_R2) && master.get_digital(DIGITAL_L2))
 		{
-			if (!(count % 500)) {
+			if (!(count % 500)){
       		
       		//master.print(0, 0, "Counter: %d", count);
     		}
