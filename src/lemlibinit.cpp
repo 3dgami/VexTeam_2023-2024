4dd8b0e1cae79755af2ce5997749aa484f20c341
3dgami <sasha-trifonov@outlook.com>
2024-02-20 17:09:19 -0800

    lemlib setup

--- src/lemlibinit.cpp
+++ src/lemlibinit.cpp
@@ -1,12 +1,21 @@
-/*#include "main.h"
+#include "main.h"
 #include "selection.h"
 #include "selection.ccp"
 #include "lemlib/api.hpp"
 
-pros::Rotation dead_left_x(7);
+/*pros::Rotation dead_left_x(7);
 pros::Rotation dead_right_x(8);
-pros::Rotation dead_y(9);
-
+pros::Rotation dead_y(9);*/
+pros::Controller master{CONTROLLER_MASTER};	
+pros::Motor right_front(20);
+pros::Motor left_front(10);
+pros::Motor left_back(1);//
+pros::Motor right_back(11);
+pros::Motor right_mid(6);
+pros::Motor left_mid(5);
+pros::Motor_Group driveL_train({left_front, left_back, left_mid});
+pros::Motor_Group driveR_train({right_front, right_back, right_mid});
+pros::IMU Inertial_Sensor(2);
 
 lemlib::Drivetrain_t drivetrain {
     &driveL_train, // left drivetrain motors
@@ -17,25 +26,28 @@ lemlib::Drivetrain_t drivetrain {
 };
 
 // Y tracking wheel
-lemlib::TrackingWheel y_tracking_wheel(&dead_y, 2.75, -4.6); // 2.75" wheel diameter, -4.6" offset from tracking center UN-MEASURED
+/*lemlib::TrackingWheel y_tracking_wheel(&dead_y, 2.75, -4.6); // 2.75" wheel diameter, -4.6" offset from tracking center UN-MEASURED
 // X tracking wheels
 lemlib::TrackingWheel x_left_tracking_wheel(&dead_right_x, 2.75, 1.7); // 2.75" wheel diameter, 1.7" offset from tracking center
-lemlib::TrackingWheel x_right_tracking_wheel(&dead_left_x, 2.75, 4.5); // 2.75" wheel diameter, 4.5" offset from tracking center UN-MEASURED
+lemlib::TrackingWheel x_right_tracking_wheel(&dead_left_x, 2.75, 4.5); // 2.75" wheel diameter, 4.5" offset from tracking center UN-MEASURED*/
  
 // odometry struct
 lemlib::OdomSensors_t sensors {
-    &y_tracking_wheel, // vertical tracking wheel 1
-    &x_left_tracking_wheel, // horizontal tracking wheel 2
-    &x_right_tracking_wheel, // horizontal tracking wheel 1
+    nullptr, // vertical tracking wheel 1
+    nullptr, // horizontal tracking wheel 2
+    nullptr, // horizontal tracking wheel 1
     nullptr, // we don't have a second tracking wheel, so we set it to nullptr
     &Inertial_Sensor // inertial sensor
 };
-
+/*
+    &y_tracking_wheel, // vertical tracking wheel 1
+    &x_left_tracking_wheel, // horizontal tracking wheel 2
+    &x_right_tracking_wheel*/
  
 // forward/backward PID
 lemlib::ChassisController_t lateralController {
-    9, // kP
-    8.5, // kD
+    10, // kP
+    30, // kD
     1, // smallErrorRange
     100, // smallErrorTimeout
     3, // largeErrorRange
@@ -45,8 +57,8 @@ lemlib::ChassisController_t lateralController {
  
 // turning PID
 lemlib::ChassisController_t angularController {
-    9, // kP
-    5.5, // kD
+    2, // kP
+    10, // kD
     1, // smallErrorRange
     100, // smallErrorTimeout
     3, // largeErrorRange
@@ -55,4 +67,4 @@ lemlib::ChassisController_t angularController {
 };
 
 // create the chassis
-lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);*/
+lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
